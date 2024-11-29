f7a85fffb31d2074ee0d83fa122d9685e021121a
nhathuy <nguyenannhathuy@gmail.com>
2024-11-29 22:34:27 +0700

    edit me again

--- front-end/src/views/Me.vue
+++ front-end/src/views/Me.vue
@@ -1,12 +1,9 @@
 <template>
     <div>
       <h1>About</h1>
-      <p>My name is Nguyen An Nhat Huy and this is my web</p>
+      <p>My name: Nguyen An Nhat Huy and this is my web</br></p>
+      <p>My banner ID: 001323809 </p>
     </div>
   </template>
   
-  <script>
-  export default {
-    name: "Me"
-  };
-  </script>
+  
