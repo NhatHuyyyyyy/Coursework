8082a92b7300461592332d736db3f659c51b33a4
nhathuy <nguyenannhathuy@gmail.com>
2024-12-05 14:04:55 +0700

    change me.vue

--- front-end/src/views/Me.vue
+++ front-end/src/views/Me.vue
@@ -1,9 +1,23 @@
 <template>
-    <div>
-      <h1>About</h1>
-      <p>My name: Nguyen An Nhat Huy and this is my web</br></p>
-      <p>My banner ID: 001323809 </p>
-    </div>
-  </template>
-  
+  <div class="introduction">
+    <h1>Hello, I'm {{ name }}!</h1>
+    
+    <p>{{ bio }}</p>
+    <p>This is my email: <a :href="'mailto:' + email">{{ email }}</a></p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Nguyen An Nhat Huy",
+      
+      bio: "This is a coursework in Course 	Web Programming 2(COMP1842) ",
+      email: "huynangcs220724@fpt.edu.vn"
+    };
+  }
+};
+</script>
+
   
